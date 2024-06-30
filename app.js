02a09c08f50216378a304de7f67ecaf4d4bba30d
Mayank <innovativecreations195@gmail.com>
2024-06-30 05:55:44 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const port = 3000;
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+let tasks = [];
+
+app.get('/', (req, res) => {
+  res.render('index', { tasks: tasks });
+});
+
