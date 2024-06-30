94e453eae91d7a356d689602038b5abe5fbb1ffb
Mayank Kumar <innovativecreations195@gmail.com>
2024-06-30 06:37:11 +0530

    basic todo

--- app.js
+++ app.js
@@ -14,3 +14,24 @@ app.get('/', (req, res) => {
   res.render('index', { tasks: tasks });
 });
 
+app.post('/addtask', (req, res) => {
+  let task = req.body.newtask;
+  tasks.push(task);
+  res.redirect('/');
+});
+
+app.post('/removetask', (req, res) => {
+  let completedTask = req.body.check;
+  if (typeof completedTask === 'string') {
+    tasks.splice(tasks.indexOf(completedTask), 1);
+  } else if (typeof completedTask === 'object') {
+    for (let i = 0; i < completedTask.length; i++) {
+      tasks.splice(tasks.indexOf(completedTask[i]), 1);
+    }
+  }
+  res.redirect('/');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
